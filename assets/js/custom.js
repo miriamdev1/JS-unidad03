d448e1875397ad201e975b869d2af257a54e13aa
miriam <getre4dy@gmnail.com>
2021-06-08 04:01:54 -0300

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,20 @@
+!(function($) {
+  "use strict";
+  // init Isotope
+  var $projects = $('.projects').isotope({
+    itemSelector: '.project',
+    layoutMode: 'fitRows'
+});
+$(".filter-btn").click(function() {
+    var data_filter = $(this).attr("data-filter");
+    $projects.isotope({
+        filter: data_filter
+    });
+    $(".filter-btn").removeClass("active");
+    $(".filter-btn").removeClass("shadow");
+    $(this).addClass("active");
+    $(this).addClass("shadow");
+    return false;
+});
+
+})(jQuery);
